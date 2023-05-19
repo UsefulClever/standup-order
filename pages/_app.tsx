288216adebd0c63d65367956a1eaabf352d7c8d3
sujal <sujal@proofoflearn.io>
2023-05-19 13:10:33 -0400

    make plausible work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+import Script from 'next/script'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src='/stats/js/script.js'
+        data-api='/stats/api/event'
+        data-domain='randomizer.usefulclever.com' />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
